6d9cd93b666e2b08d628c474b431a7da1b58c871
hafizh1119 <hafizhkusuma2@gmail.com>
2025-06-18 18:35:41 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -30,6 +30,13 @@
         ?>
 
         <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li><!-- End Profile Nav -->
+
+        <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'produk-kategori') ? '' : 'collapsed' ?>" href="produk-kategori">
                 <i class="bi bi-receipt-cutoff"></i>
                 <span>Kategori Produk</span>
